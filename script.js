3628c6db209c0a573fe57f67dacca1dee0098bdc
Aryan Ghugare <aryanghugare75@gmail.com>
2025-12-05 22:55:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let input = document.querySelector("#fname")
+
+input.addEventListener("focus",(e)=>{
+	e.preventDefault()
+	input.value = e.target.value.toUpperCase()  ;
+	
+})
